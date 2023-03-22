2eb86b6a9cdac479ae87a8643d509321a3fb1e54
Mamadou Lamine DIAKHABY <mamadou.lamine.diakhaby20@gmail.com>
2023-03-22 15:10:06 +0100

    add password page view

---
+++ src/pages/Password.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Components/Header'
+import PasswordBody from '../Components/PasswordBody'
+import Footer from '../Components/Footer'
+
+function Password() {
+  return (
+    <div>
+        <Header/>
+        <PasswordBody/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Password
