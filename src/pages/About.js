d5cb194a55ee6dd3677148d9efb0ecf5ebbdc64e
Mamadou Lamine DIAKHABY <mamadou.lamine.diakhaby20@gmail.com>
2023-03-22 14:20:36 +0100

    add about page view

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Components/Header'
+import AboutBody from '../Components/AboutBody'
+import Footer from '../Components/Footer'
+
+function About() {
+  return (
+    <div>
+        <Header/>
+        <AboutBody/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default About
