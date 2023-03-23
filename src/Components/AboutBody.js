ad8ef88e470880b300766f1fbf75ecab625dfb2f
Mamadou Lamine DIAKHABY <mamadou.lamine.diakhaby20@gmail.com>
2023-03-23 20:50:17 +0100

    update About and post page

--- src/Components/AboutBody.js
+++ src/Components/AboutBody.js
@@ -9,9 +9,10 @@ const ImageAndText = () => {
       </div>
       <div style={{ flex: 1, marginRight: 50 }}>
         <h2>A PROPOS</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam urna ac dolor suscipit, eu vestibulum nisl tristique. Quisque mollis sapien eu mauris luctus, et bibendum enim bibendum. Donec sed ante ante. </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam urna ac dolor suscipit, eu vestibulum nisl tristique. Quisque mollis sapien eu mauris luctus, et bibendum enim bibendum. Donec sed ante ante. </p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aliquam urna ac dolor suscipit, eu vestibulum nisl tristique. Quisque mollis sapien eu mauris luctus, et bibendum enim bibendum. Donec sed ante ante. </p>
+        <p>Bienvenue sur Together, un site dédié aux étudiants étrangers qui cherchent à se connecter et à s'entraider. Nous sommes ravis que vous soyez ici et nous espérons que notre plateforme sera utile pour faciliter votre intégration dans votre nouvelle communauté universitaire. </p>
+        <p>Notre objectif est de créer un espace où les étudiants étrangers peuvent échanger des conseils, des informations pratiques, des expériences et des histoires. Nous sommes convaincus que cela peut aider à briser les barrières culturelles et linguistiques qui peuvent parfois rendre l'expérience de l'étude à l'étranger difficile. </p>
+        <p>Nous sommes une équipe de passionnés qui croient en la valeur de la diversité et de l'inclusion dans l'éducation. Nous sommes constamment à la recherche de moyens d'améliorer notre plateforme pour mieux répondre aux besoins des étudiants étrangers. </p>
+        <p>Nous espérons que vous trouverez Together utile et que vous vous joindrez à notre communauté. Si vous avez des commentaires ou des suggestions, n'hésitez pas<a href='#'> à nous contacter</a>. Nous sommes là pour vous aider à vous connecter, à vous sentir chez vous et à réussir dans vos études à l'étranger.</p>
         <div style={{ marginTop : 30}}>
           <button style={{ borderRadius: '50px', backgroundColor: 'brown', padding: '10px 20px', color: 'white' }}>Témoignages</button>
         </div>
--- src/Components/PostBody.js
+++ src/Components/PostBody.js
@@ -12,21 +12,23 @@ function NouveauPoste() {
   };
 
   return (
-    <div className="nouveau-poste" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',backgroundImage: `url(${UniversityImage})` }}>
-      <h1>Nouveau poste</h1>
-      <form onSubmit={handleSubmit} style={{ width:'80%' }}>
-      <label htmlFor="category">Catégorie*:</label>
-      <select value={category} id="category" onChange={(event) => setCategory(event.target.value)} style={{ width: '80%', height: '40px', borderRadius:'50px'}}>
+    <div className="nouveau-poste" style={{  backgroundImage: `url(${UniversityImage})` }}>
+      <h1 style={{marginLeft: 30}}>Nouveau poste</h1>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems:'initial', width:'80%' }}>
+      <label style={{textAlign: 'left'}} htmlFor="category">Catégorie*:</label>
+      <select value={category} id="category" onChange={(event) => setCategory(event.target.value)} style={{ width: '80%', height: '40px', borderRadius:'50px', textAlign: 'left'}}>
         <option value="">Sélectionner une categorie</option>
         <option value="category1">category 1</option>
         <option value="category2">category 2</option>
         <option value="category3">category 3</option>
       </select> 
-        <label htmlFor="titre">Titre*</label>
-        <input type="text" id="titre" placeholder='Titre du poste' value={titre} onChange={(event) => setTitre(event.target.value)} style={{ width: '78%', borderRadius:'0px' }} />
-        <label htmlFor="post">Texte*</label>
-        <textarea id="post" value={post} rows = "5" placeholder='Développez votre besoin ...' onChange={(event) => setPost(event.target.value)} style={{ width: '79%' }}></textarea>
+        <label htmlFor="titre" style={{textAlign: 'left'}}>Titre*</label>
+        <input type="text" id="titre" placeholder='Titre du poste' value={titre} onChange={(event) => setTitre(event.target.value)} style={{ width: '78%', borderRadius:'0px', textAlign: 'left' }} />
+        <label htmlFor="post" style={{textAlign: 'left'}}>Texte*</label>
+        <textarea id="post" value={post} rows = "5" placeholder='Développez votre besoin ...' onChange={(event) => setPost(event.target.value)} style={{ width: '79%', textAlign: 'left' }}></textarea>
+        <div style={{textAlign:'left'}}>
         <button type="submit" style={{ backgroundColor: 'brown', height: 30, width: 100, marginTop:10, borderRadius:'50px' }}>Poster</button>
+        </div>
       </form>
     </div>
   );
