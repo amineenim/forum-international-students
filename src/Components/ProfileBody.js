27627d9f51f11fa3f98eeca280de5484a1ab71d2
amineenim <aminemaourid1995@gmail.com>
2023-05-03 08:44:49 +0200

    adding the possibility for a user to modify his password, with form validation, communication with backend API and notification display for both success and failure

--- src/Components/ProfileBody.js
+++ src/Components/ProfileBody.js
@@ -18,6 +18,7 @@ function ProfileBody() {
     const [authenticatedUser, setAuthenticateduser] = useState(null)
     // state that will store a notification after submission of profile edit form
     const [notification, setNotification] = useState("")
+    const [errorNotification, setErrorNotification] = useState("")
     const navigate = useNavigate()
     // verifies f the user is authenticated
     useEffect(() => {
@@ -33,7 +34,7 @@ function ProfileBody() {
     // state that stores whether the user is updating his profile or not
     const [isProfileBeingUpdated, setIsProfileBeingUpdated] = useState(false)
 
-    const {register, handleSubmit, formState : {errors}, setValue} = useForm()
+    const {register, handleSubmit, formState : {errors}, setValue, watch} = useForm()
     // validation constraints for user profile update 
     const validationConstraints = {
         email : {
@@ -82,7 +83,6 @@ function ProfileBody() {
     }
     // function that handles the form submission after validation success
     const handleProfileUpdate = async(data) => {
-        console.log(data)
         // make the Api call to update data of the user 
         try {
             const response = await ApiService.put('/users/profil',{
@@ -105,7 +105,6 @@ function ProfileBody() {
                     promotion : data.promotion
                 }))
                 setIsProfileBeingUpdated(false)
-                console.log("done")
             }
         } catch (error) {
             console.log(error)
@@ -134,6 +133,56 @@ function ProfileBody() {
         AuthService.logout()
         navigate('/login')
     }
+    // state that stores whether the user is updating his password 
+    const [isPasswordBeingUpdated, setIsPasswordBeingUpdated] = useState(false)
+    // watch the new_password and confirm_password input values 
+    const new_password = watch('new_password')
+    const confirm_password = watch('confirm_password')
+    // validation constraints for password update 
+    const passwordValidation = {
+        old_password : {required : 'le mot de passe actuel est requis'},
+        new_password : {
+            required : "champ requis",
+            pattern : {
+                value : /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^\w\s]).+$/,
+                message :'une majuscule, miniscule, chiffre et caractère spécial'
+            },
+            minLength : {
+                value : 8,
+                message : " longueur minimade de 8 caractères"
+            }
+        },
+        confirm_password : {
+            required : "confirmation requise",
+            validate : value => value === new_password || "passwords don't match"
+        }
+    }
+    // function that handles sending the password update request to the backend 
+    const handlePasswordUpdate = async(data) => {
+        setValue('old_password',"")
+        setValue('new_password',"")
+        setValue('confirm_password',"")
+        try {
+            const response = await ApiService.put('/users/password',{
+                old_password : data.old_password,
+                new_password : data.new_password,
+                confirm_password : data.confirm_password
+            })
+            if(response.status === 200 && response.statusText === "OK")
+            {
+                setNotification("Mot de passe modifié avec succès")
+                setIsPasswordBeingUpdated(false)
+            }
+        } catch (error) {
+            if(error.response.status === 400 && error.response.statusText === "BAD REQUEST")
+            {
+                setErrorNotification("Le Mot de Passe fourni est incorrect")
+            }else
+            {
+                console.log(error.response)
+            }
+        }
+    }
   return (
     <div className='big-container-profile'>
         {
@@ -150,13 +199,27 @@ function ProfileBody() {
                 </div>
             )
         }
+        {
+            errorNotification && (
+                <div className='error-update-profile'>
+                    <p>
+                        {errorNotification}
+                    </p>
+                    <span>
+                        <AiOutlineClose size={26} 
+                        onClick={() => setErrorNotification("")}
+                        />
+                    </span>
+                </div>
+            )
+        }
         <div className='profile-container'>
             <Helmet>
                 <title>TOGETHER | Profil</title>
             </Helmet>
             <div className='left-section'>
                 {
-                    (authenticatedUser && !isProfileBeingUpdated) && (
+                    (authenticatedUser && !isProfileBeingUpdated && !isPasswordBeingUpdated) && (
                         <Profile
                             image={authenticatedUser.imageUrl}
                             name = {authenticatedUser.name}
@@ -167,7 +230,7 @@ function ProfileBody() {
                     )
                 }
                 {
-                    isProfileBeingUpdated && (
+                    (isProfileBeingUpdated && !isPasswordBeingUpdated) && (
                             <form className='form-update-userprofile'
                             onSubmit={handleSubmit(handleProfileUpdate)}
                             >
@@ -252,18 +315,86 @@ function ProfileBody() {
                             </form>
                     )
                 }
+                {
+                    (isPasswordBeingUpdated && !isProfileBeingUpdated) && (
+                        <form className='update-password-user'
+                        onSubmit={handleSubmit(handlePasswordUpdate)}
+                        >
+                            <div className='row'>
+                                <p>Mot de Passe Actuel : </p>
+                                <div>
+                                    <input type='password' name='old_password' 
+                                    {...register('old_password',passwordValidation.old_password)}
+                                    />
+                                    {
+                                        errors?.old_password && (
+                                            <small className='error-text'>
+                                                {errors.old_password.message}
+                                            </small>
+                                        )
+                                    }
+                                </div>
+                            </div>
+                            <div className='row'>
+                                <p>Nouveau Mot de passe : </p>
+                                <div>
+                                    <input type='password' name='new_password' 
+                                    {...register('new_password', passwordValidation.new_password)}
+                                    />
+                                    {
+                                        errors?.new_password && (
+                                            <small className='error-text'>
+                                                {errors.new_password.message}
+                                            </small>
+                                        )
+                                    }
+                                </div>
+                            </div>
+                            <div className='row'>
+                                <p>Confirmez le Nouveau MDP : </p>
+                                <div>
+                                    <input type='password' name='confirm_password' 
+                                    {...register('confirm_password', passwordValidation.confirm_password)}
+                                    />
+                                    {
+                                        errors?.confirm_password && (
+                                            <small className='error-text'>
+                                                {errors.confirm_password.message}
+                                            </small>
+                                        )
+                                    }
+                                </div>
+                            </div>
+                            <div className='last-row'>
+                                <button type='submit' className='modify-password'>
+                                    Sauvegarder
+                                </button>
+                                <button
+                                onClick={() => {setIsPasswordBeingUpdated(false)}}
+                                >Annuler</button>
+                            </div>
+                        </form>
+                    )
+                }
             </div>
             <div className='right-section'>
                 <h3>Actions To take : </h3>
                 <div className='modify-profile'>
                     <BsFillFilePersonFill size={38} />
                     <button className='btn'
-                    onClick={() => setIsProfileBeingUpdated(true)}
+                    onClick={() => {
+                        setIsProfileBeingUpdated(true) 
+                        setIsPasswordBeingUpdated(false)
+                    }}
                     >Modifier Profile</button>
                 </div>
                 <div className='change-password'>
                     <RiLockPasswordFill size={38} />
-                    <button className='btn'>Changer Mot de passe</button>
+                    <button className='btn'
+                    onClick={() => {
+                        setIsPasswordBeingUpdated(true) 
+                        setIsProfileBeingUpdated(false)
+                        }}>Changer Mot de passe</button>
                 </div>
                 <div className='deconnecter'>
                     <FiLogOut size={38} />
